b32e227b799ca4f2460195438d2c7e0834ee7528
Kelwin Ndrianasolo <sk8kelwin@gmail.com>
2022-04-29 00:57:58 +0300

    adding shared oreferences to refine

--- lib/interface.dart
+++ lib/interface.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+late SharedPreferences prefs;
 List<String> toCopy = [
   "2021Y90200035 Ndrianasolo Arlet Kelwin",
 ];
 
-class Kopieo extends StatelessWidget {
+TextEditingController controller = TextEditingController();
+
+saveStringListValue(List<String> addit) async {
+  prefs = await SharedPreferences.getInstance();
+  prefs.setStringList("copyList", addit);
+}
+
+retrieveStringListValue() async {
+  prefs = await SharedPreferences.getInstance();
+  if (prefs.getStringList("copyList") != null) {
+    List<String> copyList = prefs.getStringList("copyList")!;
+    toCopy = copyList;
+  }
+}
+
+class Kopieo extends StatefulWidget {
   const Kopieo({Key? key}) : super(key: key);
+
+  @override
+  State<Kopieo> createState() => _KopieoState();
+}
+
+class _KopieoState extends State<Kopieo> {
+  @override
+  void initState() {
+    // saveStringListValue(toCopy);
+    // retrieveStringListValue();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,16 +47,80 @@ class Kopieo extends StatelessWidget {
       body: SingleChildScrollView(
         child: SizedBox(
           height: MediaQuery.of(context).size.height,
-          child: ListView.builder(
-            itemCount: 1,
-            itemBuilder: (BuildContext context, int index) {
-              return ListTile(
-                onTap: () {
-                  Clipboard.setData(ClipboardData(text: toCopy[index]));
-                },
-                title: Text(toCopy[index]),
-              );
-            },
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 0,
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                child: Stack(
+                  children: [
+                    SizedBox(
+                      child: ListView.builder(
+                        itemCount: 1,
+                        itemBuilder: (BuildContext context, int index) {
+                          return ListTile(
+                            onTap: () {
+                              Clipboard.setData(
+                                  ClipboardData(text: toCopy[index]));
+                            },
+                            title: Text(toCopy[index]),
+                          );
+                        },
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 100,
+                      child: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: Container(
+                                  width: 300,
+                                  height: 50,
+                                  color: Colors.orange,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: TextField(
+                                      controller: controller,
+                                      autocorrect: true,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              InkWell(
+                                onTap: (() {
+                                  print(controller.text);
+                                  toCopy.add(controller.text);
+                                  print(toCopy);
+                                  setState(() {});
+                                }),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(10),
+                                  child: Container(
+                                    width:
+                                        MediaQuery.of(context).size.width / 6,
+                                    height: 50,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
